177401438c45c69e5c06a80908736af1d27ae4ff
RoyChuChun <roy.shulman@gmail.com>
2016-11-17 19:08:55 +0200

    first commit

---
+++ constants.py
@@ -0,0 +1,7 @@
+#This is the projects constants file
+
+PROJECT_NAME_TO_ID = {"first-project": "ajsjhn2"}
+KEYSTONE_DOMAIN = "admin"
+
+#ADMIN CREDENTIALS FOR PROJECT
+
---
+++ main.py
@@ -0,0 +1,13 @@
+import easygui
+
+
+def main():
+    title = "Openstack Virtualization Platform"
+    choices = ["New project", "Existing project(add a user)"]
+    button = easygui.buttonbox("Choose an option", title=title, choices=choices)
+    if button == "New project":
+        print "a"
+
+
+if __name__ == "__main__":
+    main()
