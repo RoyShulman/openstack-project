00b806af53b307f0c3e817119de0fa46335f7df8
RoyChuChun <roy.shulman@gmail.com>
2016-11-20 21:41:45 +0200

    authentication works for v2.0

--- constants.py
+++ constants.py
@@ -4,7 +4,5 @@ PROJECT_NAME_TO_ID = {"first-project": "ajsjhn2"}
 KEYSTONE_DOMAIN = "admin"
 
 #ADMIN CREDENTIALS FOR PROJECT
-#TODO:change to controller ip in hostnames
-ADMIN_AUTH_URL = "127.0.0.1:35357/v3.0"
-#TODO: add admin token
-ADMIN_TOKEN = ""
+ADMIN_AUTH_URL = "http://192.168.1.154:35357/v2.0"
+ADMIN_TOKEN = "f363e22443526541b7a5"
--- keystone_functions.py
+++ keystone_functions.py
@@ -1,26 +1,33 @@
-from keystoneclient.v3 import client
-from keystoneauth1.identity import v3
+from keystoneclient.v2_0 import client
+from keystoneauth1.identity import v2
 from keystoneauth1 import session
 import constants
 
 class Keystone:
     def __init__(self, auth_url, user_name, password, project_name, project_description):
+        #TODO: change to a real keystone check
         if project_name in constants.PROJECT_NAME_TO_ID:
             #TODO: add functions for user to do
             #The project name exists returning none
             return None
         else:
             #The user is creating a new project
-            auth = v3.Token(auth_url=constants.ADMIN_AUTH_URL, token=constants.ADMIN_TOKEN)
+            auth = v2.Password(auth_url=constants.ADMIN_AUTH_URL, password="password",
+                               tenant_name="admin", username="admin")
             sess = session.Session(auth=auth)
             self.keystone_client = client.Client(session=sess)
-            self.project_id = self.create_project(project_name=project_name, project_description=project_description)
-            self.add_user(self, name=user_name, password=password, project=self.project_id)
+            tenants = self.keystone_client.tenants.list()
+            print tenants
+            project_id = [x.id for x in tenants if x.name == 'Demo'][0]
+            tenant = self.keystone_client.tenants.get(project_id)
+            self.keystone_client.tenants.delete(tenant)
+            print self.keystone_client.tenants.list()
+
 
     def create_project(self, project_name, project_description):
-        project_id = client.projects.ProjectManager(self.keystone_client).create(name=project_name,
-                                                                                 domain=constants.KEYSTONE_DOMAIN,
-                                                                                 description=project_description).id
+        project_id = self.keystone_client.tenants.create(tenant_name=project_name,
+                                                         description=project_description,
+                                                         enabled=True)
         return project_id
 
     def add_user(self, name, password, project):
