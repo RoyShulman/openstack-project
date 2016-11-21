0bcd8abc93503d7b872a11f63e75d3fe662f92b0
RoyChuChun <roy.shulman@gmail.com>
2016-11-21 10:27:15 +0200

    Added existing project support and refactored code

--- keystone_functions.py
+++ keystone_functions.py
@@ -4,37 +4,41 @@ from keystoneauth1 import session
 import easygui
 import constants
 
-#TODO: add a login function to not repeat code
 
 class Keystone:
-    def __init__(self, auth_url, user_name, password, project_name, project_description, type):
-        if type == "New":
-            #User wants to create a new project
-            auth = v2.Password(auth_url=constants.ADMIN_AUTH_URL, username="admin",
-                               password="password", tenant_name="admin")
-            sess = session.Session(auth=auth)
-            self.keystone_client = client.Client(session=sess)
-            project_id = [x.id for x in self.keystone_client.tenants.list() if x.name == project_name]
-            if project_id:
-                easygui.msgbox("Project with this name already exists!")
-                exit(1)
-            else:
-                auth = v2.Password(auth_url=constants.ADMIN_AUTH_URL, username="admin",
-                               password="password", tenant_name="admin")
-                sess = session.Session(auth=auth)
-                self.keystone_client = client.Client(session=sess)
-                project_id = self.keystone_client.tenants.create(tenant_name=project_name,
-                                                                 description=project_description).id
-                #create the user associated with the tenant
-
-                self.add_user(name=user_name, password=password, project_id=project_id)
-                print self.keystone_client.users.list()
+    def __init__(self, user_name, password, project_name, project_description):
+        #User wants to create a new project
+        self.keystone_client = self.password_login()
+        project_id = [x.id for x in self.keystone_client.tenants.list() if x.name == project_name]
+        if project_id:
+            easygui.msgbox("Project with this name already exists!")
+            exit(1)
+        else:
+            project_id = self.keystone_client.tenants.create(tenant_name=project_name,
+                                                             description=project_description).id
+            #create the user associated with the tenant
+            self.add_user(name=user_name, password=password, project_id=project_id)
+            print self.keystone_client.users.list()
+    def __init__(self, user_name, password, project_name):
+            #user wants to login to an existing project with his user name
+            self.keystone_client = self.password_login(auth_url=constants.ADMIN_AUTH_URL,
+                                                       username=user_name,
+                                                       password=password,
+                                                       project_name=project_name)
 
     def create_project(self, project_name, project_description):
         project_id = self.keystone_client.tenants.create(tenant_name=project_name,
                                                          description=project_description,
                                                          enabled=True)
         return project_id
+
+    def password_login(self, auth_url=constants.ADMIN_AUTH_URL, username=constants.ADMIN_USERNAME,
+                       password=constants.ADMIN_PASSWORD, project_name=constants.ADMIN_TENANT):
+        """return a keystone client by authenticating with password"""
+        auth = v2.Password(auth_url=auth_url, username=username, password=password, tenant_name=project_name)
+        sess = session.Session(auth=auth)
+        keystone_client = client.Client(session=sess)
+        return keystone_client
 
     def add_user(self, name, password, project_id):
         """function to add a new user to a project"""
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import constants
 
 def main():
     title = "Openstack Virtualization Platform"
-    choices = ["New project", "Existing project(add a user)"]
+    choices = ["New project", "Existing project"]
     button = easygui.buttonbox("Choose an option", title=title, choices=choices)
     if button == "New project":
         project_name = easygui.enterbox(msg="Enter your new project name")
@@ -20,15 +20,27 @@ def main():
         user_password = easygui.enterbox(msg="Create a password")
         if user_password is None:
             exit(1)
-        keystone_client = Keystone(auth_url=constants.ADMIN_AUTH_URL, user_name=user_name,
+        keystone_client = Keystone(user_name=user_name,
                                    password=user_password, project_name=project_name,
                                    project_description=project_description,
-                                   type="New")
+)
         #print keystone_client.project_id
         if keystone_client is None:
-            print "here"
-            print keystone_client
             easygui.msgbox("Error!\nProject with this name already exists")
+    elif button == "Existing project":
+        project_name = easygui.enterbox(msg="Enter your new project name")
+        if project_name is None:
+            exit(1)
+        user_name = easygui.enterbox(msg="Enter your username")
+        if user_name is None:
+            exit(1)
+        user_password = easygui.passwordbox(msg="Enter your password")
+        if user_password is None:
+            exit(1)
+        keystone_client = Keystone(user_name=user_name,
+                                   password=user_password,
+                                   project_name=project_name)
+
 
 
 if __name__ == "__main__":
