735ad8b748558f8538ef778d3c30fed593822cb0
RoyChuChun <roy.shulman@gmail.com>
2016-12-13 14:30:44 +0200

    added security groups

--- neutron_functions.py
+++ neutron_functions.py
@@ -8,3 +8,6 @@ class Neutron:
 
     def list_networks(self):
         return self.neutron_client.list_networks()
+
+    def create_external_network(self):
+        self.neutron_client.create_ext()#TODO:work on this
--- nova_functions.py
+++ nova_functions.py
@@ -2,6 +2,7 @@ from novaclient import client as novaClient
 import constants
 import os
 import time
+import easygui
 
 
 class Nova:
@@ -22,14 +23,14 @@ class Nova:
         instance = self.nova_client.servers.find(id=instance_id)
         return instance
 
-    def create_instance(self, image_name, instance_name):
+    def create_instance(self, image_name, instance_name, security_group_id):
         if not self.nova_client.keypairs.findall(name="mykey"):
             with open(os.path.expanduser('~/.ssh/id_rsa.pub')) as fpubkey:
                 self.nova_client.keypairs.create(name="mykey", public_key=fpubkey.read())
         image = self.nova_client.images.find(name=image_name)
         flavor = self.nova_client.flavors.find(name="m1.tiny")#because I am using VMs
         instance = self.nova_client.servers.create(name=instance_name, image=image_name,
-                                                   flavor=flavor, key_name="mykey")
+                                                   flavor=flavor, key_name="mykey", security_group=security_group_id)
 
         status = instance.status
         while status == "BUILD":
@@ -38,6 +39,7 @@ class Nova:
             status = instance.status
             print "Building"
 
+        instance.add_floating_ip()#TODO: add function to add a floating ip
         print "Finished!"
 
     def create_security_group(self):
@@ -47,3 +49,12 @@ class Nova:
                                                 from_port=22, to_port=22)#SSH
         self.nova_client.security_groups_rules.create(security_group.id, ip_protocol="icmp",
                                                 from_port=-1, to_port=-1)#ICMP
+        return security_group
+
+    def get_instance(self, instance_name):
+        instance_id= [x.id for x in self.list_images() if x.name == instance_name]
+        if instance_id == []:
+            easygui.msgbox("Instance name could not be found")
+            exit(1)
+        else:
+            return instance_id
