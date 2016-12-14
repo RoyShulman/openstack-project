b8c6fd9b929a330b35b11995ad09722141269a06
RoyChuChun <roy.shulman@gmail.com>
2016-12-14 22:26:33 +0200

    reset network steps added - needs implementation

--- neutron_functions.py
+++ neutron_functions.py
@@ -11,3 +11,12 @@ class Neutron:
 
     def create_external_network(self):
         self.neutron_client.create_ext()#TODO:work on this
+
+    def reset_network(self):
+        """1. router gateway clear
+           2. router interface delete subnets
+           3. router delete
+           4. delete all floating ips
+           5. delete subnets
+           6. delete netsg
+        """
