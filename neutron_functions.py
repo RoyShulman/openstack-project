c8c210fd3a3c06d6d996876fff4759dc3ab1c697
RoyChuChun <roy.shulman@gmail.com>
2017-01-17 12:51:27 +0200

    Added neutron documentation

--- neutron_functions.py
+++ neutron_functions.py
@@ -7,11 +7,24 @@ class Neutron:
         self.neutron_client = neutronClient.Client(session=keystone_session)
 
     def list_networks(self):
+        """
+        List all Networks
+        """
         return self.neutron_client.list_networks()
 
     def create_network(self, network_name, is_external, network_type):
+        """
+        Create a network
+        :param network_name: The name of the network to create
+        :param is_external: Boolean for if the network is external
+        :param network_type: Which network type to create
+        """
         network = {'name': network_name, 'router:external': is_external, 'provider:network_type':network_type}
         self.neutron_client.create_network({'network':network})
 
     def delete_network(self, network_id):
+        """
+        Delete a network
+        :param network_id: Unique ID of the network to delete
+        """
         self.neutron_client.delete_network(network=network_id)
