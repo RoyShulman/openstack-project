da78f6ed498757ed31c445fabba635d0ab0c01ba
RoyChuChun <roy.shulman@gmail.com>
2017-01-17 13:33:21 +0200

    Added glance documentation

--- glance_functions.py
+++ glance_functions.py
@@ -10,6 +10,9 @@ class Glance:
         self.glance_client = glanceClient.Client(version='2', session=keystone_session)
 
     def create_image(self):
+        """
+        Create a new image file
+        """
         #add options for different images
         image_url = easygui.enterbox(msg="Enter the image url(Nothing for the default)")
         if image_url == "":#user wants to add the default images
@@ -23,15 +26,36 @@ class Glance:
         self.glance_client.images.upload(image.id, vm_image.read())
 
     def get_image(self, image_id):
+        """
+        Return an glance image type
+        :param image_id: The unique ID of the image
+        :return: glance.image
+        """
         return self.glance_client.images.get(image_id)
 
     def list_images(self):
+        """
+        List all available images
+        :return: List of all images
+        """
         return self.glance_client.images.list()
 
     def delete_image(self, image_id):
-        self.glance_client.images.delete(image_id)
+        """
+        Delete an given image file
+        :param image_id: The unique ID of the image to delete
+        """
+        try:
+            self.glance_client.images.delete(image_id)
+        except Exception, e:
+            easygui.msgbox("Something went wrong please try again")
+            print e
+            return
 
     def add_default_images(self):
+        """
+        Add all the default images in the constants file
+        """
         for image_name in constants.IMAGE_URLS:
             vm_image = urllib2.urlopen(constants.IMAGE_URLS[image_name])
             print "adding image: ", image_name
